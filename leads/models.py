cb148d62f7a9a8f9c85bc634b30f95d0dfef1c8b
harshhx <hg070401@gmail.com>
2021-11-25 02:30:03 +0530

    lead model fix

--- leads/models.py
+++ leads/models.py
@@ -9,9 +9,15 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Leads(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
