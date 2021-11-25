78acc5c0a540a7120f40dd3735e4ce5769313a9e
harshhx <hg070401@gmail.com>
2021-11-26 00:09:20 +0530

    each lead detail view and initialisation of create lead

--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Lead
 
 
@@ -7,3 +8,12 @@ def lead_list(request):
     leads = Lead.objects.all()
 
     return render(request, 'leads/lead_list.html', {'leads': leads})
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    return render(request, 'leads/lead_detail.html', {'lead': lead})
+
+
+def lead_create(request):
+    return render(request, 'leads/lead_create.html', {})
