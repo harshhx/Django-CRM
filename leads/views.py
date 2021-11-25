86a17b2439de16004eae92540b847e18376cc17b
harshhx <hg070401@gmail.com>
2021-11-26 00:29:21 +0530

    leads generation

--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 
 def lead_list(request):
-
     leads = Lead.objects.all()
 
     return render(request, 'leads/lead_list.html', {'leads': leads})
@@ -16,4 +16,18 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    return render(request, 'leads/lead_create.html', {})
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            agent = Agent.objects.first()
+            lead = Lead(
+                first_name=data.get('first_name'),
+                last_name=data.get('last_name'),
+                age=data.get('age'),
+                agent=agent
+            )
+            lead.save()
+            return redirect('/leads/')
+    return render(request, 'leads/lead_create.html', {'form': form})
