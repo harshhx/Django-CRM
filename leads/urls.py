eafbfcb410d7f20e2eb69fb21ec6712e06201607
harshhx <hg070401@gmail.com>
2021-11-27 23:11:07 +0530

    fix

--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import (lead_detail, lead_create, lead_update, lead_delete, LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView)
+from .views import (LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView)
 
 app_name = 'leads'
 
