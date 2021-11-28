fdf58ade03fc1b865726fa7a6fe8271573203d8f
harshhx <hg070401@gmail.com>
2021-11-29 00:54:33 +0530

    agents initialised with listing of agents

--- crm/urls.py
+++ crm/urls.py
@@ -1,15 +1,18 @@
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
-from leads.views import LandingView
+from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+
+from leads.views import LandingView
 from leads.views import SignUpView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', landing, name='landing'),
     path('', LandingView.as_view(), name='landing'),
     path('leads/', include('leads.urls', namespace='leads')),
+    path('agents/', include('agents.urls', namespace='agents')),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup')
